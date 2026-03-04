ee4cda010d4eec8fc1d150f8cf6e01e77619699e
Konstantina Magalou <konnamagalou@gmail.com>
2026-03-04 14:43:55 +0200

    deleted columns

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# 5g-6g-project
+Αρχικά στο preprocessing.py διαγράφουμε τις περιττές στήλες, δηλαδή τη στήλη id και attack_cat.
--- preprocessing.py
+++ preprocessing.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
-# 1. Φόρτωση ΜΟΝΟ του train set
-train_df = pd.read_csv('"C:\Users\ldako\Downloads\UNSW_NB15_training-set(in).csv"')
+# 1. Φόρτωση του train set (EXCEL, όχι CSV)
+train_df = pd.read_excel(r"C:\Users\ldako\Downloads\UNSW_NB15_training-set(in).xlsx")
 
 print("Το train set φορτώθηκε! Αρχικό μέγεθος:", train_df.shape)
 
 # 2. Διαγραφή των στηλών 'id' και 'attack_cat'
-# axis=1 σημαίνει ότι διαγράφουμε στήλες 
 train_df = train_df.drop(['id', 'attack_cat'], axis=1)
 
 print("Οι στήλες διαγράφηκαν! Νέο μέγεθος:", train_df.shape)
 
-# 3. Εμφάνιση των πρώτων 5 γραμμών για οπτικό έλεγχο
-train_df.head()
+# 3. Εμφάνιση των πρώτων 5 γραμμών
+print(train_df.head())
