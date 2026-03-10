e37fa2261e8a090309010f36760c6a5deb864b2f
ANTONIS TSIPOULAKOS <antonistsipoulakos@ANTONISs-Mac-mini.local>
2026-03-10 18:28:28 +0200

    Πρόσθεσα το νέο αρχείο

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # 1. Φόρτωση ΜΟΝΟ του train set
-train_df = pd.read_csv('"C:\Users\ldako\Downloads\UNSW_NB15_training-set(in).csv"')
+train_df = pd.read_csv('/Users/antonistsipoulakos/Downloads/UNSW_NB15_training-set(in).csv')
 
 print("Το train set φορτώθηκε! Αρχικό μέγεθος:", train_df.shape)
 
@@ -12,4 +12,33 @@ train_df = train_df.drop(['id', 'attack_cat'], axis=1)
 print("Οι στήλες διαγράφηκαν! Νέο μέγεθος:", train_df.shape)
 
 # 3. Εμφάνιση των πρώτων 5 γραμμών για οπτικό έλεγχο
-train_df.head()
+print(train_df.head())
+
+
+
+
+
+#ΕΠΟΜΕΝΕΣ ΑΛΛΑΓΕΣ
+
+# 1. Διαχωρισμός των χαρακτηριστικών (X) από τον στόχο (y)
+X_train = train_df.drop('label', axis=1)
+y_train = train_df['label']
+
+print("Ο διαχωρισμός σε X_train και y_train έγινε επιτυχώς!")
+
+# 2. Έλεγχος για κενές τιμές (Βήμα 'α' της εκφώνησης)
+# Το συγκεκριμένο αρχείο συνήθως είναι καθαρό, αλλά καλό είναι να το ελέγχουμε
+missing_values = X_train.isnull().sum().sum()
+print(f"Συνολικές κενές τιμές στο X_train: {missing_values}")
+
+# Εάν missing_values > 0, θα χρειαζόταν συμπλήρωση (π.χ. με X_train = X_train.fillna(0)). 
+# Λογικά θα σου βγάλει 0.
+
+# 3. Μετατροπή ποιοτικών χαρακτηριστικών σε 0/1 (Dummy Variables - Βήμα 'β')
+categorical_cols = ['proto', 'service', 'state']
+
+# Η εντολή pd.get_dummies φτιάχνει αυτόματα τις νέες στήλες με τα 0/1
+X_train = pd.get_dummies(X_train, columns=categorical_cols)
+
+print("Οι ποιοτικές μεταβλητές μετατράπηκαν σε Dummies!")
+print("Νέο μέγεθος X_train (παρατηρούμε ότι οι στήλες αυξήθηκαν σημαντικά):", X_train.shape)
